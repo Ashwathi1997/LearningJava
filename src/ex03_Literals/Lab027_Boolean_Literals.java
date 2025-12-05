3b9c9f2a1f88c72cc28927bfa3dc345af094e02d
Ashwathi1997 <57622638+Ashwathi1997@users.noreply.github.com>
2025-12-06 02:10:24 +0530

    Delete src/ex03_Literals directory

--- src/ex03_Literals/Lab027_Boolean_Literals.java
+++
@@ -1,9 +0,0 @@
-package ex03_Literals;
-
-public class Lab027_Boolean_Literals {
-    public static void main(String[] args) {
-        boolean is_married = true;
-        boolean is_married_priya = false;
-        //boolean is_married_priya = 123; does not allow it
-    }
-}
