491fc28d9288fbda74a159ded51ea99078c915b8
Ashwathi1997 <ashwathi.mohanan7@gmail.com>
2025-12-17 12:45:44 +0530

    Coding challenge 1

---
+++ src/ex_02_Java_Practise_Tasks/Lab025_CodingC1_Declare_Initialize.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Practise_Tasks;
+
+public class Lab025_CodingC1_Declare_Initialize {
+    public static void main(String[] args) {
+        int Integer = 25;
+        double Double = 3.14;
+        boolean Boolean = true;
+        char Character = 'A';
+
+        System.out.print("Integer: "+Integer);
+        System.out.print(" Double: "+Double);
+        System.out.print(" Boolean: "+Boolean);
+        System.out.print(" Character: "+Character);
+    }
+}
