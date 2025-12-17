5b6ee93173183fdfdeb50aebc2281ac528a6b70e
Ashwathi1997 <ashwathi.mohanan7@gmail.com>
2025-12-17 12:54:17 +0530

    Coding challenge 1

---
+++ src/ex_02_Java_Practise_Tasks/Lab027_CodingC2_Arith_Assign_Op.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Practise_Tasks;
+
+public class Lab027_CodingC2_Arith_Assign_Op {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        int add = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int mod = a % b;
+
+        System.out.printf("Addition: %d Subtraction: %d " +
+                "Multiplication: %d Division: %d " +
+                "Modulus: %d", add, sub, mul, div, mod);
+
+
+    }
+}
