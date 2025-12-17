c11022f471a95a0044f68708343221e8781bb1b0
Ashwathi1997 <ashwathi.mohanan7@gmail.com>
2025-12-18 00:06:46 +0530

    Coding challenge 6

---
+++ src/ex_02_Java_Practise_Tasks/Lab031_CodingC6_No_Classification.java
@@ -0,0 +1,23 @@
+package ex_02_Java_Practise_Tasks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Lab031_CodingC6_No_Classification {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check if it is positive or negative: ");
+        int input = sc.nextInt();
+
+        if(input>=0)
+        {
+            System.out.println(input+" is positive");
+        }
+        else
+        {
+            System.out.println(input+" is negative");
+        }
+    }
+}
