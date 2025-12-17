c2b7fdc77e013f6f7d969e907249e00db032eb99
Ashwathi1997 <ashwathi.mohanan7@gmail.com>
2025-12-17 13:20:03 +0530

    Coding challenge 4

---
+++ src/ex_02_Java_Practise_Tasks/Lab029_CodingC4_Incre_Decre_Op.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Practise_Tasks;
+
+public class Lab029_CodingC4_Incre_Decre_Op {
+    public static void main(String[] args) {
+        int i = 5;
+
+        System.out.print("Original: "+i+" Pre-increment: "+ ++i +
+                " Post-increment: "+ i++ +" (returned 6) Pre-decrement: "+ (--i) +
+                " Post-decrement: " + (i--) +"returned 5");
+    }
+}
